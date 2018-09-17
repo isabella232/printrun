31f9631bb2b26bc6255aa2ca640924b57eab310a
Jordan Miller <jordan@volumetricbio.com>
2018-09-16 21:06:09 -0500

    test.py will run test.gcode, home z-axis, and then increment in z. The printer is left connected

---
+++ test.gcode
@@ -0,0 +1,13 @@
+G28 Z ; Home Z-axis until endstop is hit
+G91 ; Relative Coordinates
+G0 Z1 ; move up 1
+G4 P2000; wait 2 sec
+G0 Z1 ; move up 1
+G4 P2000; wait 2 sec
+G0 Z1 ; move up 1
+G4 P2000; wait 2 sec
+G0 Z1 ; move up 1
+G4 P2000; wait 2 sec
+G0 Z1 ; move up 1
+G4 P2000; wait 2 sec
+G0 Z1 ; move up 1
---
+++ test.py
@@ -0,0 +1,37 @@
+#to send a file of gcode to the printer
+from printrun.printcore import printcore
+from printrun import gcoder
+import time
+
+# jmil says FIND THE CORRECT PORT FIRST WITH "ls /dev/tty.usb*"
+
+p=printcore("/dev/tty.usbmodem144241",115200) # or p.printcore('COM3',115200) on Windows
+
+while not p.online: time.sleep(0.1)
+
+gcode=[i.strip() for i in open('test.gcode')] # or pass in your own array of gcode lines instead of reading from a file
+gcode = gcoder.LightGCode(gcode)
+p.startprint(gcode) # this will start a print
+
+
+# p.disconnect() # this is how you disconnect from the printer once you are done. This will also stop running prints.
+
+
+
+
+
+
+# Kliment default file with printcore, from: https://github.com/kliment/Printrun#cython-based-g-code-parser
+# #to send a file of gcode to the printer
+# from printrun.printcore import printcore
+# from printrun import gcoder
+# p=printcore('/dev/ttyUSB0',115200) # or p.printcore('COM3',115200) on Windows
+# gcode=[i.strip() for i in open('filename.gcode')] # or pass in your own array of gcode lines instead of reading from a file
+# gcode = gcoder.LightGCode(gcode)
+# p.startprint(gcode) # this will start a print
+#
+# #If you need to interact with the printer:
+# p.send_now("M105") # this will send M105 immediately, ahead of the rest of the print
+# p.pause() # use these to pause/resume the current print
+# p.resume()
+# p.disconnect() # this is how you disconnect from the printer once you are done. This will also stop running prints.
